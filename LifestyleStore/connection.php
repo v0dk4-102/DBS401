f34708f934e62e717e1a68687dde59d45a1f6ed4
liujang <91556806+Liuchijang@users.noreply.github.com>
2023-09-29 10:11:53 +0700

    Update connection.php

--- LifestyleStore/connection.php
+++ LifestyleStore/connection.php
@@ -1,4 +1,4 @@
 <?php
-$con=mysqli_connect("localhost","id2538044_sajalagrawal","******","id2538044_store") or die(mysqli_error($con));
+$con=mysqli_connect("localhost","group6","5up3rSecur3.P4s5w0rd(>?","dbs401") or die(mysqli_error($con));
 //$con=mysqli_connect("localhost","root","","store") or die(mysqli_error($con));
 ?>
