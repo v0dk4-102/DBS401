e798fe603c3b773fb4ee5daab119452baee1fd69
liujang <91556806+Liuchijang@users.noreply.github.com>
2023-10-09 21:30:07 +0700

    Update settings.php
    a

--- LifestyleStore/settings.php
+++ LifestyleStore/settings.php
@@ -32,9 +32,9 @@
                     <div class="col-xs-4 col-xs-offset-4">
                         <h1>Change Password</h1>
                         <form method="post" action="setting_script.php">
-                            <div class="form-group">
+<!--                             <div class="form-group">
                                 <input type="password" class="form-control" name="oldPassword" placeholder="Old Password">
-                            </div>
+                            </div> -->
                             <div class="form-group">
                                 <input type="password" class="form-control" name="newPassword" placeholder="New Password">
                             </div>
