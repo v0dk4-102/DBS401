4f95f254ecb4adeaf59c58a4157cdc744b96e83b
liujang <91556806+Liuchijang@users.noreply.github.com>
2023-10-09 21:04:35 +0700

    Update login.php

--- LifestyleStore/login.php
+++ LifestyleStore/login.php
@@ -35,10 +35,10 @@
                                 <p>Login to make a purchase.</p>
                                 <form method="post" action="login_submit.php">
                                     <div class="form-group">
-                                        <input type="email" class="form-control" name="email" placeholder="Email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$">
+                                        <input type="email" class="form-control" name="email" placeholder="Email">
                                     </div>
                                     <div class="form-group">
-                                        <input type="password" class="form-control" name="password" placeholder="Password(min. 6 characters)" pattern=".{6,}">
+                                        <input type="password" class="form-control" name="password"">
                                     </div>
                                     <div class="form-group">
                                         <input type="submit" value="Login" class="btn btn-primary">
