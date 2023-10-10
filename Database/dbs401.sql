392777230e7507219a47314371dede2762ba0ab5
liujang <91556806+Liuchijang@users.noreply.github.com>
2023-10-10 10:07:46 +0700

    Update dbs401.sql

--- Database/dbs401.sql
+++ Database/dbs401.sql
@@ -77,9 +77,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `contact`, `city`, `address`) VALUES
-(1, 'Sajal', 'sajal.agrawal1997@gmail.com', '57f231b1ec41dc6641270cb09a56f897', '8899889988', 'Indore', '100 palace colony, Indore'),
-(2, 'Ram', 'ram1234@xyz.com', '57f231b1ec41dc6641270cb09a56f897', '8899889989', 'Pune', '100 palace colony, Pune'),
-(3, 'Shyam', 'shyam@xyz.com', '57f231b1ec41dc6641270cb09a56f897', '8899889990', 'Bangalore', '100 palace colony, Bangalore');
+(1, 'Sajal', 'sajal.agrawal1997@gmail.com', 'abc123', '8899889988', 'Indore', '100 palace colony, Indore'),
+(2, 'Ram', 'ram1234@xyz.com', 'abc123', '8899889989', 'Pune', '100 palace colony, Pune'),
+(3, 'Shyam', 'shyam@xyz.com', 'abc123', '8899889990', 'Bangalore', '100 palace colony, Bangalore');
 
 -- --------------------------------------------------------
 
