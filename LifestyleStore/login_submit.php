457a7c53b57e8b02fd69aad7f095147b54229bbe
liujang <91556806+Liuchijang@users.noreply.github.com>
2023-10-10 08:33:56 +0700

    Update login_submit.php

--- LifestyleStore/login_submit.php
+++ LifestyleStore/login_submit.php
@@ -1,7 +1,7 @@
 <?php
     require 'connection.php';
     session_start();
-    $email=mysqli_real_escape_string($con,$_POST['email']);
+    $email=$_POST['email'];
     $regex_email="/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[_a-z0-9-]+)*(\.[a-z]{2,3})$/";
     if(!preg_match($regex_email,$email)){
         echo "Incorrect email. Redirecting you back to login page...";
@@ -9,7 +9,7 @@
         <meta http-equiv="refresh" content="2;url=login.php" />
         <?php
     }
-    $password=md5(md5(mysqli_real_escape_string($con,$_POST['password'])));
+    $password=md5(md5($_POST['password']));
     if(strlen($password)<6){
         echo "Password should have atleast 6 characters. Redirecting you back to login page...";
         ?>
