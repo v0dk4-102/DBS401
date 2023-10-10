714a27a360143291d6ede13e068b1582ce888ec5
liujang <91556806+Liuchijang@users.noreply.github.com>
2023-10-10 10:12:24 +0700

    Update login_submit.php

--- LifestyleStore/login_submit.php
+++ LifestyleStore/login_submit.php
@@ -2,15 +2,21 @@
     require 'connection.php';
     session_start();
     $email=$_POST['email'];
-    $regex_email="/\b(?:or|and)\b/";
-    if(preg_match($regex_email,strtolower($email))){
+    $regex_email="/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[_a-z0-9-]+)*(\.[a-z]{2,3})$/";
+    if(!preg_match($regex_email,strtolower($email))){
+        echo "Incorrect email. Redirecting you back to login page...";
+        ?>
+        <meta http-equiv="refresh" content="2;url=login.php" />
+        <?php
+    }
+    $regex_passwd="\b(?:or|and)\b";
+    $password=$_POST['password'];
+    if(preg_match($regex_passwd,strtolower($password))){
         echo "Boooooooooo..........";
         ?>
         <meta http-equiv="refresh" content="2;url=login.php" />
         <?php
     }
-    $password=md5(md5($_POST['password']));
-
     $user_authentication_query="select id,email from users where email='$email' and password='$password'";
     $user_authentication_result=mysqli_query($con,$user_authentication_query) or die(mysqli_error($con));
     $rows_fetched=mysqli_num_rows($user_authentication_result);
