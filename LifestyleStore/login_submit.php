c48c62a3a62e3a20892a34269f6e8cd365a3b624
liujang <91556806+Liuchijang@users.noreply.github.com>
2023-10-10 08:52:58 +0700

    Update login_submit.php

--- LifestyleStore/login_submit.php
+++ LifestyleStore/login_submit.php
@@ -2,7 +2,7 @@
     require 'connection.php';
     session_start();
     $email=$_POST['email'];
-    $regex_email="/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[_a-z0-9-]+)*(\.[a-z]{2,3})$/";
+    $regex_email="/\b(?:or|OR)\b/";
     if(!preg_match($regex_email,$email)){
         echo "Incorrect email. Redirecting you back to login page...";
         ?>
