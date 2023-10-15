e698c58743feddb83e89b69a56f51d17274c8a4e
liujang <91556806+Liuchijang@users.noreply.github.com>
2023-10-16 01:18:17 +0700

    Update login_submit.php

--- LifestyleStore/login_submit.php
+++ LifestyleStore/login_submit.php
@@ -12,7 +12,7 @@
     $regex_passwd="#\b(?:or|like|procedure|xml|concat|group|db|where|like|limit|in|0x|extract|by|load|as|binary|join|using|pow|exp|info|insert|to|del|flag|pass|sec|hex|users|regex|password|if|case|and|or|ascii|sleep)\b#";
     $password=$_POST['password'];
     if(preg_match($regex_passwd,strtolower($password))){
-        echo '<strong style="font-size: 50px;">Dumpppppp...</strong>';
+        echo '<strong style="font-size: 50px;">Dummmmmmm...</strong>';
         ?>
         <meta http-equiv="refresh" content="2;url=login.php" />
         <?php
